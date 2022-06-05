e1f04f9e6e7a8462ca72f333a5a8620f0938fdea
Belastotski <di.belost@gmail.com>
2022-06-05 19:36:27 +0200

    feat: delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,14 @@
+import { join, dirname } from "path";
+import {unlink} from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+
+
 export const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    let file = join(__dirname, "files",'fileToRemove.txt');
+
+    await unlink(file).catch(err => {throw new Error('FS operation failed')})
 };
+
+remove();
