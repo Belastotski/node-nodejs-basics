ecf7e8e9e0d7766b2ccffb876ed58645a08310ad
Belastotski <di.belost@gmail.com>
2022-06-05 20:17:35 +0200

    feat: read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
+import { join, dirname, parse } from "path";
+import {readFile} from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+
+
 export const read = async () => {
-    // Write your code here 
+    
+    const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+let file = join(__dirname, "files",'fileToRead.txt');
+await readFile(file, 'utf8')
+.then(content => process.stdout.write(content))
+.catch(err => {throw new Error('FS operation failed')}); 
 };
+
+read();
