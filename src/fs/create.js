f3164a8799f886f72dade70eb53d209b8f1e0871
Belastotski <di.belost@gmail.com>
2022-06-05 18:17:09 +0200

    init: start task 1

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,18 @@
+import {stat, access, writeFile} from 'node:fs/promises';
+// import {createWriteStream} from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    let file = path.join(__dirname,'files','fresh.txt');
+    access(file).then( _ => {throw new Error('FS operation failed')})
+    .catch( e => { 
+        e.code == "ENOENT"
+        ?writeFile(file, 'I am fresh and young')
+        :console.error(e);
+    })
 };
+
+create();
